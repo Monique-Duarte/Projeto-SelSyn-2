1a51f77fd914312eb66e355cb5b6bffa67d661fc
Monique Duarte <moniquead95@gmail.com>
2025-02-04 17:42:58 -0300

    css

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,17 @@
-import { useState } from 'react'
-import './App.css'
-import Login from './Components/Login/Login';
+import "./App.css";
+import Login from "./Components/Login/Login";
+import Welcome from "./Components/Welcome/Welcome";
+import { Routes, Route } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <Login/>
-    </div>
+    <main className="main">
+      <Routes>
+        <Route path="/" Component={Login} />
+        <Route path="/bem-vindo" Component={Welcome} />
+      </Routes>
+    </main>
   );
 }
 
-export default App
+export default App;
--- src/Components/Login/Login.jsx
+++ src/Components/Login/Login.jsx
@@ -1,43 +1,42 @@
-import {FaUser, FaLock} from 'react-icons/fa'
+import { FaUser, FaLock } from "react-icons/fa";
 import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
 import "./Login.css";
 
 const Login = () => {
+    const navigate = useNavigate();
     const [username, setUsername] = useState("");
     const [password, setPassword] = useState("");
 
     const handleLogin = () => {
-        if (username === "admin" && password === "1234") {
-        alert("Bem Vindo");
-        } else {
+    if (username === "admin" && password === "1234") {
+        navigate("/bem-vindo");
+    } else {
         alert("Credenciais inválidas!");
-        }
+    }
     };
 
     return (
     <div className="container">
-        <h1>Acesse o Sistema</h1>
-        <form onSubmit = {handleLogin}>
-            <div>
-                <input type="name" placeholder='Login' required onChange = {(e) => setUsername(e.target.value)}/>
-                <FaUser className='icon'/>
-            </div>
-            <div>
-                <input type="password" placeholder='Senha'required onChange = {(e) => setPassword(e.target.value)}/>
-                <FaLock className='icon'/>
-            </div>
-
-            <div className="recall-forget">
-                <label>
-                    <input type="checkbox" />
-                    Lembre de mim
-                </label>
-                <a href="#">Esqueceu a senha?</a>
-            </div>
-        <button>Acessar</button>
-        </form>
+            <form onSubmit={handleLogin}>
+            <h1>Acesse o Sistema</h1>
+                <div className="input-field">
+                    <input type="name" placeholder="Login" required onChange={(e) => setUsername(e.target.value)} /> <FaUser className="icon"/> 
+                </div>
+                <div className="input-field">
+                    <input type="password" placeholder="Senha" required onChange={(e) => setPassword(e.target.value)} /> <FaLock className="icon"/>
+                </div>
+                <div className="recall-forget">
+                    <label>
+                    <input type="checkbox" /> Lembre de mim 
+                    </label>
+                    <a href="#">Esqueceu a senha?</a>
+                </div>
+            <button>Acessar</button>
+            </form>
     </div>
-    )
-}
+    );
+};
 
-export default Login
+export default Login;
